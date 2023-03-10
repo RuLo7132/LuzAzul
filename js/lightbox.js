b8977e427092b6d7de19ace7b4229648d26ee010
HarleySantos10 <127469466+HarleySantos10@users.noreply.github.com>
2023-03-09 22:59:04 -0600

    Add files via upload

---
+++ js/lightbox.js
@@ -0,0 +1,28 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenesLight = document.querySelector('.agregar-imagen')
+const contenedorLight = document.querySelector('.imagen-light')
+const inicio1 = document.querySelector('.inicio');
+ 
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+    })
+})
+
+
+contenedorLight.addEventListener('click', (e)=>{
+    if(e.target !== imagenesLight){
+        contenedorLight.classList.toggle('show')
+        imagenesLight.classList.toggle('showImage')
+        inicio1.style.opacity = '1'
+    }
+})
+
+
+const aparecerImagen = (imagen)=>{
+     imagenesLight.scr = imagen
+     contenedorLight.classList.toggle('show')
+     imagenesLight.classList.toggle('showImage')
+     inicio1.style.opacity = '0'
+} 
